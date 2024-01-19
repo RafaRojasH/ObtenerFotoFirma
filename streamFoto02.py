71f2e3231b9e56acc93d471ab9967a319f004970
RafaRojasH <94881844+RafaRojasH@users.noreply.github.com>
2024-01-19 12:42:56 -0600

    Update streamFoto02.py

--- streamFoto02.py
+++ streamFoto02.py
@@ -6,8 +6,8 @@ import math
 import cv2
 from io import BytesIO
 
-
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+#face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 #if face_cascade.empty(): raise Exception("¿Está seguro que es la ruta correcta?")
 
 #@st.experimental_singleton
